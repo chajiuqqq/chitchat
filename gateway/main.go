23c65645c13e366b4bb8dcb9106c7dac6e07d6fd
Chris <chris.cai@vimworld.com>
2023-06-01 10:00:40 +0800

    update

--- gateway/main.go
+++ gateway/main.go
@@ -12,22 +12,22 @@ import (
 	"strings"
 	"syscall"
 
-	"github.com/hashicorp/consul/api"
-	"github.com/chajiuqqq/chitchat/gateway/handler"
+	"github.com/afex/hystrix-go/hystrix"
 	"github.com/chajiuqqq/chitchat/common/discover"
 	"github.com/chajiuqqq/chitchat/common/loadbalance"
+	"github.com/chajiuqqq/chitchat/gateway/handler"
+	"github.com/hashicorp/consul/api"
 )
 
 var (
-	gatwayPort = flag.String("port", "8000", "gateway port")
-	consulHost = flag.String("consul-host", "0.0.0.0", "consul host")
-	consulPort = flag.String("consul-port", "8500", "consul port")
+	gatwayPort  = flag.String("port", "8000", "gateway port")
+	metricsPort = flag.String("metricsPort", "8500", "metrics port")
 )
 
 func main() {
 	flag.Parse()
 	discoverClient := discover.NewConsulClient()
-	myProxy := handler.NewHystrixHandler(discoverClient,loadbalance.NewRandomLoadBalance())
+	myProxy := handler.NewHystrixHandler(discoverClient, loadbalance.NewRandomLoadBalance())
 	errChan := make(chan error)
 	go func() {
 		c := make(chan os.Signal)
@@ -38,6 +38,12 @@ func main() {
 	go func() {
 		log.Println("gateway start at port", *gatwayPort)
 		http.ListenAndServe(":"+*gatwayPort, myProxy)
+	}()
+	go func() {
+		handler := hystrix.NewStreamHandler()
+		handler.Start()
+		http.Handle("/hystrix/stream", handler)
+		http.ListenAndServe(":"+*metricsPort, nil)
 	}()
 	log.Println("gateway exit", <-errChan)
 }
